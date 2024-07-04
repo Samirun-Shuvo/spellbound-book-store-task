c571df1497ef6122881353700d082c598a584438
Samirun-Shuvo <samirunshuvo@gmail.com>
2024-07-04 14:09:43 +0600

    responsive

--- src/pages/home/experience/Experience.jsx
+++ src/pages/home/experience/Experience.jsx
@@ -9,31 +9,53 @@ const Experience = () => {
       <div className="hero opacity-70 w-full">
         <div className="hero-content flex-col lg:flex-row-reverse w-full">
           <div className="text-center lg:text-left lg:mr-20 lg:max-w-lg w-full">
-            <h1 className="text-5xl font-bold mb-6">
-              Premium <span className="text-orange-500">Learning</span> Experience
+            <h1 className="text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-5 md:mb-6">
+              Premium <span className="text-orange-500">Learning</span>{" "}
+              Experience
             </h1>
+
             <div className="flex justify-center lg:justify-start items-center mb-4">
-              <div className="relative bg-cover bg-center p-2" style={{ backgroundImage: `url(${rectangle})` }}>
+              <div
+                className="relative bg-cover bg-center p-2"
+                style={{ backgroundImage: `url(${rectangle})` }}
+              >
                 <img className="w-16 sm:w-20" src={heart} alt="heart" />
               </div>
               <div className="px-2 text-left">
-                <h2 className="text-md font-semibold py-1">Easily Accessible</h2>
-                <p className="text-xs text-gray-600">Learning Will feel Very Comfortable With Courslab.</p>
+                <h2 className="text-md font-semibold py-1">
+                  Easily Accessible
+                </h2>
+                <p className="text-xs text-gray-600">
+                  Learning Will feel Very Comfortable With Courslab.
+                </p>
               </div>
             </div>
             <div className="flex justify-center lg:justify-start items-center">
-              <div className="relative bg-cover bg-center p-2" style={{ backgroundImage: `url(${rectangle})` }}>
+              <div
+                className="relative bg-cover bg-center p-2"
+                style={{ backgroundImage: `url(${rectangle})` }}
+              >
                 <img className="w-16 sm:w-20" src={jigsaw} alt="jigsaw" />
               </div>
               <div className="px-2 text-left">
-                <h2 className="text-md font-semibold py-1">Fun Learning Experience</h2>
-                <p className="text-xs text-gray-600">Learning Will feel Very Comfortable With Courslab.</p>
+                <h2 className="text-md font-semibold py-1">
+                  Fun Learning Experience
+                </h2>
+                <p className="text-xs text-gray-600">
+                  Learning Will feel Very Comfortable With Courslab.
+                </p>
               </div>
             </div>
+
+            
           </div>
           <div className="card bg-base-100 w-full mt-8 lg:mt-0">
-          <img src={expImg} alt="Experience Header" className="w-3/5 h-auto mx-auto" />
-        </div>
+            <img
+              src={expImg}
+              alt="Experience Header"
+              className="w-3/5 h-auto mx-auto"
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/shared/footer/Footer.jsx
+++ src/pages/shared/footer/Footer.jsx
@@ -4,50 +4,52 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div>
-      <footer className="max-w-screen-xl mx-auto footer grid lg:grid-cols-6 gap-5 text-base-content py-10">
-        <div className="grid col-span-2 lg:pr-24">
-          <img src={logo1} alt="Company Logo" className="w-28" />
-          <p className="text-justify">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industrys standard dummy text
-            ever since the 1500s.
-          </p>
-        </div>
-        <div>
-          <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About Us</a>
-          <a className="link link-hover">How to work?</a>
-          <a className="link link-hover">Populer Course</a>
-          <a className="link link-hover">Service</a>
-        </div>
-        <div>
-          <h6 className="footer-title">Courses</h6>
-          <a className="link link-hover">Categories</a>
-          <a className="link link-hover">Ofline Course</a>
-          <a className="link link-hover">Vidio Course</a>
-        </div>
-        <div>
-          <h6 className="footer-title">Support</h6>
-          <a className="link link-hover">FAQ</a>
-          <a className="link link-hover">Help Center</a>
-          <a className="link link-hover">Career</a>
-          <a className="link link-hover">Privacy </a>
-        </div>
-        <div>
-          <h6 className="footer-title">Contac Info</h6>
-          <a className="link link-hover">+0913-705-3875</a>
-          <a className="link link-hover">ElizabethJ@jourrapide.com</a>
-          <a className="link link-hover">
-            4808 Skinner Hollow Road Days Creek, OR 97429
-          </a>
+    <div className="overflow-hidden">
+      <footer className="max-w-screen-xl mx-auto grid gap-5 py-10 px-5 lg:px-0 text-base-content">
+        <div className="lg:flex justify-between items-start">
+          <div className="flex flex-col lg:w-1/3 lg:pr-24 mb-8 lg:mb-0">
+            <img src={logo1} alt="Company Logo" className="w-28 mb-5" />
+            <p className="text-justify">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry’s standard dummy text
+              ever since the 1500s.
+            </p>
+          </div>
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5 w-full lg:w-2/3">
+            <div>
+              <h6 className="footer-title mb-4">Company</h6>
+              <p className="link link-hover">About Us</p>
+              <p className="link link-hover">How to work?</p>
+              <p className="link link-hover">Popular Course</p>
+              <p className="link link-hover">Service</p>
+            </div>
+            <div>
+              <h6 className="footer-title mb-4">Courses</h6>
+              <p className="link link-hover">Categories</p>
+              <p className="link link-hover">Offline Course</p>
+              <p className="link link-hover">Video Course</p>
+            </div>
+            <div>
+              <h6 className="footer-title mb-4">Support</h6>
+              <p className="link link-hover">FAQ</p>
+              <p className="link link-hover">Help Center</p>
+              <p className="link link-hover">Career</p>
+              <p className="link link-hover">Privacy</p>
+            </div>
+            <div>
+              <h6 className="footer-title mb-4">Contact Info</h6>
+              <p className="link link-hover">+0913-705-3875</p>
+              <p className="link link-hover break-all">ElizabethJ@jourrapide.com</p>
+              <p className="link link-hover break-all">
+                4808 Skinner Hollow Road Days Creek, OR 97429
+              </p>
+            </div>
+          </div>
         </div>
       </footer>
       <hr className="w-full h-0.5 bg-[#dad7d7]" />
-      <footer className="max-w-screen-xl mx-auto footer footer-center text-base-content p-4">
-        <aside>
-          <p>Spellbound &copy; {currentYear}. All Rights Reserved.</p>
-        </aside>
+      <footer className="max-w-screen-xl mx-auto text-center text-base-content py-4">
+        <p>Spellbound &copy; {currentYear}. All Rights Reserved.</p>
       </footer>
     </div>
   );
